bd1365a2fd5bc2008685579efa4c767a76faa7ec
Elena Neroslavskaya <lenisha@gmail.com>
2020-05-18 19:50:46 -0400

    working JDBC logs to files

--- startup.sh
+++ startup.sh
@@ -6,6 +6,6 @@ echo "JAVA OPTS ====== $JAVA_OPTS  ===="
 #export CATALINA_OPTS="-Djava.io.tmpdir=/mnt/azure/${MY_POD_NAME} $CATALINA_OPTS"
 echo "CATALINA_OPTS ===== $CATALINA_OPTS ======"
 
-mkdir /mnt/azure/${MY_POD_NAME}  
+#mkdir /mnt/azure/${MY_POD_NAME}  
 # start the tomcat
 $CATALINA_HOME/bin/catalina.sh run
